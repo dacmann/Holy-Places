721ac4511eccc116cbdf6875259f82dfb39110c0
Derek Cordon <derek.cordon@macbookpro15dc.local>
2017-06-08 12:54:47 -0400

    cleaned up extraneous code

--- Holy Places/Temples.swift
+++ Holy Places/Temples.swift
@@ -29,21 +29,20 @@ class Temple: NSObject {
     var coordinate : CLLocationCoordinate2D
 
     init(Name:String!, Address:String!, Snippet:String!, CityState:String!, Country:String!, Phone:String!, Latitude:Double!, Longitude:Double!, Order:Int16, PictureURL:String!, SiteURL:String!, Type:String!){
-        self.templeName = Name
-        self.templeAddress = Address
-        self.templeSnippet = Snippet
-        self.templeCityState = CityState
-        self.templeCountry = Country
-        self.templePhone = Phone
-        self.templeLatitude = Latitude
-        self.templeLongitude = Longitude
-        self.templeOrder = Order
-        self.templePictureURL = PictureURL
-        self.templeType = Type
-        self.cllocation = CLLocation(latitude: Latitude!, longitude: Longitude!)
-        self.coordinate = CLLocationCoordinate2D(latitude: Latitude!, longitude: Longitude!)
-        //self.distance = distance
-        self.templeSiteURL = SiteURL
+        templeName = Name
+        templeAddress = Address
+        templeSnippet = Snippet
+        templeCityState = CityState
+        templeCountry = Country
+        templePhone = Phone
+        templeLatitude = Latitude
+        templeLongitude = Longitude
+        templeOrder = Order
+        templePictureURL = PictureURL
+        templeType = Type
+        cllocation = CLLocation(latitude: Latitude!, longitude: Longitude!)
+        coordinate = CLLocationCoordinate2D(latitude: Latitude!, longitude: Longitude!)
+        templeSiteURL = SiteURL
     }
     
     
