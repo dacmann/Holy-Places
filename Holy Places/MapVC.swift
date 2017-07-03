54ef43c5d5cbecead281a34206d696a79e47c5e1
Derek Cordon <derek.cordon@macbookpro15dc.local>
2017-07-03 16:38:27 -0400

    - Added Sat view option to map - Added navigation button to map callout

--- Holy Places/MapVC.swift
+++ Holy Places/MapVC.swift
@@ -20,9 +20,27 @@ class MapVC: UIViewController, MKMapViewDelegate {
 
         // Do any additional setup after loading the view.
         self.configureView()
-        let button = UIBarButtonItem(title: "All", style: .plain, target: self, action: #selector(showAll(_:)))
-//        let mapButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(showAll(_:)))
+        // Add Show All button on right side of navigation bar
+        let button = UIBarButtonItem(title: "Show All", style: .plain, target: self, action: #selector(showAll(_:)))
+        let options = ["Map", "Sat"]
         self.navigationItem.rightBarButtonItem = button
+        let mapOptions = UISegmentedControl(items: options)
+        mapOptions.selectedSegmentIndex = 0
+        mapOptions.addTarget(self, action: #selector(changeMap(_:)), for: .valueChanged)
+        let attr = NSDictionary(object: UIFont(name: "Baskerville", size: 14.0)!, forKey: NSFontAttributeName as NSCopying)
+        mapOptions.setTitleTextAttributes(attr as? [AnyHashable : Any], for: .normal)
+        self.navigationItem.titleView = mapOptions
+        // Show the user current location
+        mapView.showsUserLocation = true
+    }
+    
+    func changeMap(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 1:
+            mapView.mapType = .satellite
+        default:
+            mapView.mapType = .standard
+        }
     }
     
     func showAll(_ sender: Any) {
@@ -40,7 +58,7 @@ class MapVC: UIViewController, MKMapViewDelegate {
             mapCenter = mapPoints[0].coordinate
             mapView.setCenter(mapCenter, animated: false)
             mapView.selectAnnotation(mapPoints[0], animated: true)
-            let mapCamera = MKMapCamera(lookingAtCenter: mapCenter, fromEyeCoordinate: mapCenter, eyeAltitude: 2000)
+            let mapCamera = MKMapCamera(lookingAtCenter: mapCenter, fromEyeCoordinate: mapCenter, eyeAltitude: 4000)
             mapView.setCamera(mapCamera, animated: false)
         }
     }
@@ -68,6 +86,13 @@ class MapVC: UIViewController, MKMapViewDelegate {
         } else { //make a new view
             view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotation.title)
         }
+        // Right accessory view
+        //        let image = UIImage(#imageLiteral(resourceName: "nav"))
+        let button = UIButton(type: .custom)
+        button.setTitle("⤴️", for: .normal)
+        //        button.setImage(UIImage.init(named: "nav"), for: UIControlState())
+        button.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        view.rightCalloutAccessoryView = button
         view.isEnabled = true
         view.canShowCallout = true
         view.pinTintColor = pinColor(type: annotation.type)
@@ -76,6 +101,14 @@ class MapVC: UIViewController, MKMapViewDelegate {
 
     // MARK: - Navigation
 
-
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        let placemark = MKPlacemark(coordinate: view.annotation!.coordinate, addressDictionary: nil)
+        // The map item is the place location
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = (view.annotation?.title)!
+        print(view.annotation?.title as Any)
+        let launchOptions = [MKLaunchOptionsDirectionsModeKey:MKLaunchOptionsDirectionsModeDriving]
+        mapItem.openInMaps(launchOptions: launchOptions)
+    }
 
 }
