1b1a1a883d08c78d4fd7befefdf04194df0e98a2
Derek Cordon <dacmann@icloud.com>
2018-11-07 11:29:51 -0500

    - Preparing for release; re-enabled the rating prompt

--- Holy Places/SummaryVC.swift
+++ Holy Places/SummaryVC.swift
@@ -609,7 +609,7 @@ class SummaryVC: UIViewController, NSFetchedResultsControllerDelegate, XMLParser
             // If entered a few visits, prompt for a rating
             if visitCnt > 3 {
                 if #available(iOS 10.3, *) {
-//                    SKStoreReviewController.requestReview()
+                    SKStoreReviewController.requestReview()
                 }
             }
           
